837fab240f30f80244e461c17715e725a37cb616
Prakashkumar <38753475+prakashkumarnks@users.noreply.github.com>
2019-11-15 17:11:21 +0530

    Add files via upload

--- src/App.js
+++ src/App.js
@@ -3,31 +3,26 @@ import { BrowserRouter as Router, Route, Link  } from "react-router-dom";
 import login from './Component/login';
 import home from './Component/home';
 import myform from './Component/myform';
-import PropData from './Component/props';
-import dropdown from './Component/dropdown';
 import view from './Component/view';
 import lifecycle from './Component/lifecycle'; 
-
+import { PrivateRoute } from './Component/PrivateRoute';
 
 class App extends React.Component {
 	render() {
 		return (
-
+/*path="/user/manage(/:id)(/:type)"  path="/myform/:postId" */
 			<div className="Apps">
 				<Router> <div className="menu">
 					<Route path="/" exact component={login} />
-					<Route path="/home" exact component={home} />
-					<Route path="/props_data" exact component={PropData} />
-					<Route path="/dropdown" exact component={dropdown} />
-					<Route path="/view" exact component={view} />		
-					<Route path="/myform/:postId" exact component={myform} />	
+					<PrivateRoute path="/home" exact component={home} />
+				
+					<PrivateRoute path="/view" exact component={view} />		
+					<Route path="/myform/:postId/:secpar" exact component={myform} />	
+					
 					<Route path="/myform" exact component={myform} />
 					<Route path="/lifecycle" exact component={lifecycle} />
 					<ul>
-						<li> <Link to="/home">home</Link> </li>
-						<li> <Link to="/">login</Link> </li>
-						<li> <Link to="/PropData">props_data</Link> </li>
-						<li> <Link to="/dropdown">dropdown</Link> </li>
+						<li> <Link to="/home">home</Link> </li>		
 						<li> <Link to="/view">view</Link> </li>
 						<li> <Link to="/myform">myform</Link> </li>
 						<li> <Link to="/lifecycle">lifecycle</Link> </li>
