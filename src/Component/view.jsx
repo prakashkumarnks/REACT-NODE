f1a829cd1c0d5f13b54add1973aebc5e87dfb871
Prakashkumar <38753475+prakashkumarnks@users.noreply.github.com>
2019-11-26 15:00:26 +0530

    Add files via upload

--- src/Component/view.jsx
+++ src/Component/view.jsx
@@ -1,9 +1,7 @@
 import React from 'react';
 import { Link } from "react-router-dom";
 import parse from 'html-react-parser';
-import { httpverbsInsertForm, SessionNotSet } from './Common';
-
-
+import { httpverbspost,httpverbsInsertForm,paginationlink,PerPagelimit } from './Common';
 class view extends React.Component {
 
 	constructor(props) {
@@ -12,29 +10,55 @@ class view extends React.Component {
 			data: '',
 			droplets: [],
 			pages: "",
+			totalCount : "",
+			start : 0,
 		}
 	}
+	
+	
 	componentDidMount() {
-
-			httpverbsInsertForm('/editupdate').then(response => {
-				this.setState({ droplets: response.data.data, pages: response.data.pages });
-			})
+	  //  console.log(PerPagelimit);
+		/*httpverbsInsertForm('/editupdate').then(response => {
+			this.setState({ droplets: response.data.data, pages: response.data.pages });
+		})
 			.catch(error => {
 				console.log(error);
-			});
+			});*/
 	}
+
+
+	 handlePageClick = data => {
+	     //alert(data.selected);
+	     console.log(data.selected);
+	     
+	     httpverbspost('/editupdate',{ start : data.selected }).then(response => {
+	         
+	       //  console.log(response.totalCount);
+	       var   start =   (data.selected) * 10;
+	         if (start < 0)
+	             start = 0;
+	            this.setState({ droplets: response.data , totalCount : response.totalCount , start : data.selected * 10 + 1 });
+	        })
+	            .catch(error => {
+	                console.log(error);
+	            });
+	   };
+	   
+
 
 
 	render() {
-		const { droplets, pages } = this.state;
-
+		const { droplets, pages , getresult ,totalCount} = this.state;
+		 
+      var i = this.state.start;
+      console.log(i);
 		return (
 			<div className="container">
-				{SessionNotSet()}
+		        { paginationlink(this.handlePageClick,totalCount) }
 				<table className="table">
 					<thead>
 						<tr>
-
+		                    <th>S.No</th>
 							<th>ID</th>
 							<th>b</th>
 							<th>c</th>
@@ -43,21 +67,22 @@ class view extends React.Component {
 					</thead>
 					<tbody>
 						{
+		                        
 							droplets.map((user) => {
 								return (
 									<tr>
-										<td> {user.a} </td>
+										<td>  { i++ } </td>
+										<td>  {user.a} </td>
 										<td>  {user.b} </td>
 										<td>  {user.c} </td>
-										<td><Link to={`/myform/${user.a}`}>Edit</Link></td>
+										<td><Link to={`/myform/${user.a}/${user.b}`}>Edit</Link></td>
 									</tr>
 								)
 							})
 						}
 					</tbody>
 				</table>
-				{parse(pages)}
-
+							
 			</div>
 		)
 	}
