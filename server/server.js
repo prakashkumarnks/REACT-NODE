a7f0793d5a144231839935eb97e2ae362ba4868a
Prakashkumar <38753475+prakashkumarnks@users.noreply.github.com>
2019-12-11 12:37:41 +0530

    Add files via upload

--- server/server.js
+++ server/server.js
@@ -1,40 +1,21 @@
 var express = require('express');
-var app = express();
-// const fileUpload = require('express-fileupload');
 var bodyParser = require('body-parser');
 var cookieParser = require('cookie-parser');
 var session = require('express-session');
 var multer = require('multer');
+var formidable = require('formidable');
+var fs = require('fs');
 var path = require('path');
 var app = express();
 
-app.use(cookieParser());
-app.use(session({ secret: "Shh, its a secret!" }));
 
-// for parsing application/json
 app.use(bodyParser.json());
 
-// for parsing application/xwww-
-app.use(bodyParser.urlencoded({ extended: true }));
-// form-urlencoded
-var upload = multer();
-// for parsing multipart/form-data
-app.use(upload.array());
 app.use(express.static('public'));
-
-
-// default options
-// app.use(fileUpload());
-
 
 const Model = require('./Model/QueryDb');
 let Db = new Model();
 
-app.use(session({
-	secret: 'ffff',
-	resave: true,
-	saveUninitialized: true
-}));
 
 
 
@@ -59,21 +40,102 @@ app.use(function (req, res, next) {
 	next();
 });
 
-
-
 /*
- * var storage = multer.diskStorage({ destination: function (req, file,
- * callback) { // let dest; // console.log(dest);
- * 
- * var uploadPath = __dirname + '/uploadmultiple/'; callback(null, uploadPath); },
- * filename: function (req, file, callback) { callback(null, file.fieldname +
- * '-' + Date.now()); } });
+upload.fields([{
+           name: 'video', maxCount: 1
+         }, {
+           name: 'subtitles', maxCount: 1
+         }])
+         
+         
+         upload.fields([{ name: 'video', maxCount: 1}, {name: 'subtitles', maxCount: 1}])
  */
 
-
-
-
 var common = require('./Common/common');
+
+
+var storage = multer.diskStorage({
+	  destination: function (req, file, cb) {
+	    cb(null, 'E:/reactlogin/server/uploadmultiple/')
+	  },
+	  filename: function (req, file, cb) {
+	    cb(null, file.fieldname + '-' + Date.now())
+	  }
+	})
+	
+var upload = multer({ storage: storage  })
+ //console.log(upload);
+//var upload = multer({ dest: 'E:/reactlogin/server/uploadmultiple/' });
+//console.log(upload);
+
+
+//let upload = multer({ storage: storage,dest: 'E:/reactlogin/server/uploads/' });
+
+//app.post('/imageuppppp', upload.single('userPhoto'), (req, res, next) => {   // correct
+//app.post('/imageuppppp', upload.single('userPhoto'),upload.single('filetoupload'), (req, res, next) => {
+app.post('/imageuppppp', upload.fields([{ name: 'userPhoto', maxCount: 2,pam: 'one'}, {name: 'filetoupload', maxCount: 2 ,pam: 'two'}]), (req, res, next) => {  // correct
+  const file = req.file;
+  
+  console.log(req.body.a);
+  
+ /* if (!file) {
+    const error = new Error('Please upload a file')
+    error.httpStatusCode = 400
+    return next(error)
+  }*/
+    res.send(file)
+  
+})
+
+app.post('/s', upload.none(), function (req, res, next) {
+//app.post('/s', function (req, res) {
+//app.post('/s',  (req, res, next) => {  // correct
+  
+	///var upload = multer({ dest: 'E:/reactlogin/server/uploadmultiple/' });
+	///var p = upload.fields([{ name: 'userPhoto', maxCount: 2 }]);
+
+	  console.log(req.body.a);
+
+	  res.end(req.body.a);
+	  
+	/*p(req, res, function (err) {
+
+		///console.log(req.files);
+		if (err) {
+			return res.end("Error uploading file.");
+		}
+		res.end("File is uploaded");
+	});*/
+	
+	
+	/*	var sec = multer({ dest: 'E:/reactlogin/server/uploads/' });
+	var secll = sec.fields([{ name: 'filetoupload', maxCount: 2 }]);
+	
+	secll(req, res, function (err) {
+
+		console.log(req.files);
+		
+	})*/
+})
+/*
+app.post('/imageuppppp', function (req, res) {
+	
+	var commonobj = new common();
+	
+	var s = commonobj.storage('userPhoto', 1, '/uploads/', req, res);
+	s(req, res, function (err) {
+
+		console.log(req.files);
+		if (err) {
+			return res.end("Error uploading file.");
+		}
+		res.end("File is uploaded");
+	});
+	res.end("File is uploaded");
+});
+*/
+
+
 
 app.post('/uploadmultiple', function (req, res) {
 	// var upload = multer({ storage : storage }).array('userPhoto',2);
@@ -87,7 +149,7 @@ app.post('/uploadmultiple', function (req, res) {
 	// var p = multer({ storage: userPhoto }).array('userPhoto', 2);
 	// var m = multer({ storage: uploadmultiple }).array('uploadmultiple', 3);
 	// console.log(imagename);
-	var p = upload.fields([{ name: 'userPhoto', maxCount: 2 }, { name: 'uploadmultiple', maxCount: 3 }])
+	var p = upload.fields([{ name: 'userPhoto', maxCount: 2 }, { name: 'filetoupload', maxCount: 3 }])
 
 	// console.log(p);
 
@@ -161,12 +223,15 @@ app.post('/statevsdistict', function (req, res, next) {
 	});
 });
 
-
-app.post('/myform', function (req, res, next) {
+app.post('/myform', upload.fields([{ name: 'filesss', maxCount: 1 ,  p : 1  } ]),  (req, res, next) => {  // correct
+///app.post('/myform', function (req, res, next) {
 	
+//	res.send("File is uploaded");
 	var a = req.body.a;
 	var b = req.body.b;
 	var RfId = req.body.RfId;
+	
+	console.log(req.files);
 	
 	if(!a)
 	{
@@ -194,10 +259,10 @@ app.post('/myform', function (req, res, next) {
 				  res.send({success : "success" , message:"inserted ", rowssec : rowssec });
 			 });
 		}
-});
+})
 
 
-const Pagination = require('./Controller/pagination');
+
 
 
 
@@ -205,32 +270,24 @@ const Pagination = require('./Controller/pagination');
 
 app.post('/editupdate', function (req, res, next) {
 	
-	
-	
-	
-	
-	var sql = "SELECT * FROM test";
+	let start = req.body.start;
 	let RfId = req.body.RfId;
 
+	var sql = "SELECT * FROM test";
 	if (RfId)
 		sql += ' where  a = ' + RfId + ' ';
+
+	if (!RfId)
+		sql += ' limit ' + start * 10 + ',10 ';
 		
-	 //page_id = parseInt(req.params.page),
-     //currentPage = page_id > 0 ? page_id : currentPage,
-	 currentPage = 0;
-    		 
-	//console.log(sql);
+//	console.log(sql);
 	Db.query(sql).then(rowssec => {
 		Db.numRows("SELECT *  FROM test").then(totalCount => {
-			var Paginationlink = new Pagination(totalCount,currentPage,'view',10);
-			//const Paginate = new Pagination(totalCount,currentPage,pageUri,perPage);
-			res.send({data : rowssec , totalCount : totalCount ,pages : Paginationlink.links()});
+			res.send({data : rowssec , totalCount : totalCount});
 		
 		});	
 	});
 
-	
-	
 });
 
 
@@ -240,17 +297,10 @@ app.get('/', function (req, res) {
 })
 
 
+app.post('/Fillddl', function (req, res) {
 
-
-var singleclick = require('./singleclick');
-app.post('/singleclick', function (req, res) {
-
-	//var data = singleclick.SelectRecBySp("SELECT * FROM bind_activestatus");
-
-	//var data = singleclick.myDateTime();
-	//res.send(data);
-
-	singleclick.SelectRecBySp("SELECT * FROM bind_activestatus").then(data =>{
+var Query = req.body.Query;
+	singleclick.SelectRecBySp(Query).then(data =>{
 			res.send(data);
 
 	});
@@ -259,24 +309,5 @@ app.post('/singleclick', function (req, res) {
 
 
 
-app.post('/upload', function (req, res) {
-	if (!req.files || Object.keys(req.files).length === 0) {
-		return res.status(400).send('No files were uploaded.');
-	}
-
-	// The name of the input field (i.e. "sampleFile") is used to retrieve the
-	// uploaded file
-	let sampleFile = req.files.sampleFile;
-	var uploadPath = __dirname + '/imageupload/' + sampleFile.name;
-	// Use the mv() method to place the file somewhere on your server
-	sampleFile.mv(uploadPath, function (err) {
-		if (err)
-			return res.status(500).send(err);
-
-		res.send('File uploaded!');
-	});
-});
-
-
-const PORT = process.env.PORT || 8082;
+const PORT = process.env.PORT || 8083;
 app.listen(PORT, () => console.log(`listening on ${PORT}`))
