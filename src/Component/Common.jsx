d4cb08a19977643123699bb6bb7cab45e537b698
Prakashkumar <38753475+prakashkumarnks@users.noreply.github.com>
2019-12-11 12:36:05 +0530

    Add files via upload

--- src/Component/Common.jsx
+++ src/Component/Common.jsx
@@ -3,6 +3,9 @@ import { Redirect } from 'react-router-dom';
 import axios from 'axios';
 import ReactPaginate from 'react-paginate';
 import '../Assets/pagination.css';
+import Button from 'react-bootstrap/Button';
+import * as FileSaver from 'file-saver';
+import * as XLSX from 'xlsx'
 //import Pagination from "react-js-pagination";
 
 
@@ -86,21 +89,6 @@ export function SessionNotSet() {
 	}
 }
 
-/*  <ReactPaginate
-previousLabel={'<'}
-nextLabel={'>'}
-breakLabel={'...'}
-breakClassName={'break-me'}
-pageCount={pageCount}
-marginPagesDisplayed={2}
-pageRangeDisplayed={5}
-onPageChange={handlePageClick}
-containerClassName={'pagination'}
-subContainerClassName={'pages pagination'}
-activeClassName={'active'}
-initialPage={0}
-//  disableInitialCallback = {true}
-/>*/
 
 export function  paginationlink(handlePageClick,count)
 {
@@ -127,6 +115,24 @@ export function  paginationlink(handlePageClick,count)
             />
             </div>
           );
+}
+
+
+export const ExportCSV = ({csvData, fileName}) => {
+    const fileType = 'application/vnd.openxmlformats-officedocument.spreadsheetml.sheet;charset=UTF-8';
+    const fileExtension = '.xlsx';
+
+    const exportToCSV = (csvData, fileName) => {
+        const ws = XLSX.utils.json_to_sheet(csvData);
+        const wb = { Sheets: { 'data': ws }, SheetNames: ['data'] };
+        const excelBuffer = XLSX.write(wb, { bookType: 'xlsx', type: 'array' });
+        const data = new Blob([excelBuffer], {type: fileType});
+        FileSaver.saveAs(data, fileName + fileExtension);
+    }
+
+    return (
+        <Button variant="warning" onClick={ (e) => exportToCSV(csvData,fileName)}>Export</Button>
+    )
 }
 
 
